3efdb51b3c1d4c53d3026982f1621803bb1bda23
zachcory <142504903+zachcory@users.noreply.github.com>
2023-12-07 15:19:18 -0500

    Update main.py
    In this commit we:
    Imported pdfplumber, created 3 functions to open, read and store the text from the imported pdf. I also fixed the "Read PDF" button and it is functional now. The result of the current code is able to extract the text from a pdf and store it under the pdf_text variable.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
+import pdfplumber
 import openai
 
 open.ai_api_key= 'Placeholder'
@@ -11,6 +12,18 @@ def browse_file(entry_var):
     if file_path:
         # Set the selected file path in the corresponding entry field
         entry_var.set(file_path)
+#Read the text
+def read_pdf(file_path):
+    with pdfplumber.open(file_path) as pdf:
+        #read the text
+        text_content = ""
+        for page in pdf.pages:
+            text_content += page.extract_text()
+        return text_content
+#Read the pdf
+def analyze_pdf(file_path):
+    #read the pdf
+    pdf_text=read_pdf(file_path)
 
 window = tkinter.Tk()
 window.title("Course Entry Form")
@@ -18,37 +31,33 @@ window.title("Course Entry Form")
 frame = tkinter.Frame(window)
 frame.pack()
 
-# Saving User Info
-user_info_frame = tkinter.LabelFrame(frame, text="Course Entry")
+user_info_frame = tkinter.LabelFrame(frame, text="Course PDF")
 user_info_frame.grid(row=0, column=0, padx=20, pady=10)
 
 file_path_label=tkinter.Label(user_info_frame, text="File Path")
 file_path_label.grid(row=0 , column=1)
 
-# Create entry fields and browse buttons up to 5
-for i in range(1, 6):
-    label = tkinter.Label(user_info_frame, text=str(i))
-    label.grid(row=i, column=0)
+label = tkinter.Label(user_info_frame, text="Course")
+label.grid(row=1, column=0)
 
-    entry_var = tkinter.StringVar()
-    entry = tkinter.Entry(user_info_frame, textvariable=entry_var)
-    entry.grid(row=i, column=1)
+entry_var = tkinter.StringVar()
+entry = tkinter.Entry(user_info_frame, textvariable=entry_var)
+entry.grid(row=1, column=1)
 
-    browse_button = tkinter.Button(user_info_frame, text="Browse", command=lambda entry_var=entry_var: browse_file(entry_var))
-    browse_button.grid(row=i, column=2, pady=5)
+browse_button = tkinter.Button(user_info_frame, text="Browse", command=lambda entry_var=entry_var: browse_file(entry_var))
+browse_button.grid(row=1, column=2, pady=5)
 
-for widget in user_info_frame.winfo_children():
-    widget.grid_configure(padx=10, pady=5)
-
-# Saving Course Info
 courses_frame = tkinter.LabelFrame(frame)
 courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
-
-for widget in courses_frame.winfo_children():
-    widget.grid_configure(padx=10, pady=5)
-
+#Get the file path
+def import_files():
+    #Get the file path
+    file_path = entry_var.get()
+    if file_path:
+        analyze_pdf(file_path)
+        
 # Button
-button = tkinter.Button(frame, text="Import Files", command=lambda: None)
+button = tkinter.Button(frame, text="Import Files", command=import_files)
 button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
 
 window.mainloop()
