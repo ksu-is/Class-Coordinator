78bad23112e80d6d1390366aa3f63f4ef256c460
zachcory <142504903+zachcory@users.noreply.github.com>
2023-12-08 11:35:08 -0500

    Update gsheets.py
    This is a simple, working script for putting a date and time into google sheets

--- gsheets.py
+++ gsheets.py
@@ -1,17 +1,20 @@
 import gspread
 from google.oauth2.service_account import Credentials
 # Define the scope
-scope = ['https://www.googleapis.com/auth/spreadsheets']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 # Add credentials to the account
-creds = Credentials.from_service_account_file('service_account.json', scopes=scope)
-# Authorize the clientsheet
-client = gspread.authorize(creds)
+def add_assignment(assignment_title,assignment_date):
+    creds = Credentials.from_service_account_file('service_account.json', scopes=SCOPES)
+    # Authorize the clientsheet
+    client = gspread.authorize(creds)
 
-# Open the sheet
-sheet = client.open('Class Coordinator').sheet1  # Or use .worksheet('Sheet1')
+    #sheet = client.create('Class Coordinator')
 
-# Data to be inserted
-row = ["Value1", "Value2", "Value3"]  # Update with the data you want to insert
+    # Open the sheet
+    sheet = client.open('Class Coordinator').sheet1  # Or use .worksheet('Sheet1')
 
-# Insert the row
-sheet.append_row(row)
+    # Data to be inserted
+    row = [assignment_title,assignment_date]  # Update with the data you want to insert
+
+    # Insert the row
+    sheet.append_row(row)
