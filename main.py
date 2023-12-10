00235a6bc431fd7fe49fd41c616df6b2485858ca
zachcory <142504903+zachcory@users.noreply.github.com>
2023-12-09 19:58:27 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import os
 import time
 import gcalendar
 import gsheets
+import json
+import re
 
 # Set your OpenAI API key
-os.environ['OPENAI_API_KEY'] ='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'
+os.environ['OPENAI_API_KEY'] ='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'
 client= OpenAI()
 
 def run_assistant(pdf_file):
@@ -41,38 +43,102 @@ def run_assistant(pdf_file):
     thread_id=thread.id,
     assistant_id=assistant.id,
     )
+    #Check status of assistant run. Wait for status is complete
+    while True:
+        run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+        )
+        if run.status == "completed" or run.status == "failed":
+            break
+        
     messages = client.beta.threads.messages.list(
     thread_id=thread.id
-    )
-    #time.sleep(60)
-    #print(messages.data)
+    )      
+    #extract_data(messages.model_dump_json())
+    data_string = json.dumps(messages.model_dump_json())
+    #extract_assignments_dates(data_string)
+    #print(data_string)
+#def extract_assignments_dates(json_data):
+    response_dict = json.loads(data_string)
+    # Extract the assistant's message, which presumably contains the assignments and due dates
+    # The assistant's response seems to be the first item in the 'data' list
+    assistant_message = response_dict["data"][0]["content"][0]["text"]["value"]
+
+    # Replace unnecessary characters and split the string into lines for easier processing
+    formatted_text = assistant_message.replace("\\n", "\n").replace("\\\"", "\"")
+    lines = formatted_text.split('\n')
+
+    # Initialize variables to hold semester/year and the list of assignments with due dates
+    semester_year = "Unknown"  # placeholder in case the semester/year can't be determined
+    assignments = []
+
+    # Extract the semester/year and assignments from the formatted lines
+    for line in lines:
+        if "The semester and year of the syllabus are" in line:
+            semester_year = line.split("are ")[1].rstrip(".")
+        elif "**Assignment name:" in line:
+            assignment_info = line.split("**Assignment name: ")[1].rstrip("**")
+            due_date_line = lines[lines.index(line) + 1]  # Due date is on the next line
+            due_date = due_date_line.split(": ")[1]
+            assignments.append((assignment_info, due_date))
+
+    # Display extracted information
+
+    for assignment, due_date in assignments:
+        print(assignment,due_date)
+
+
+
+
+
+    #assignments = []
+    #dates = []
+    #for item in data_string['data']:
+        #content = item.get('content', [{}])[0].get('text', {}).get('value', '')
+        #matches = re.findall(r'- \*\*Assignment name: (.*?)\*\*\\n\s+- Due date: (\d{4}-\d{2}-\d{2})', content)
+        #for match in matches:
+            #assignments.append(match[0])
+            #dates.append(match[1])
+            #gcalendar.add_calendar_event(match[0],match[1])
+            #gsheets.add_assignment(match[0],match[1])
+        #return assignments, dates
+
+
+
+
+# Example usage:
+#your_json_data = "{\"data\":[{ ... (your JSON data here) ... }]}"
+#extracted_assignments, extracted_dates = extract_assignments_dates(your_json_data)
+
+#for assignment, date in zip(extracted_assignments, extracted_dates):
+    #print(f"Assignment: {assignment.strip()}, Date: {date}")
+
+    #print(messages.model_dump_json)
+    # Extract the last message, which we'll assume contains our relevant data.
+    #extraction_message = messages.data[-1].content
+    #split into individual lines
+    
+    
+    #assignments=[]
+    #dates=[]
+
+    #for line in extraction_message:
+        #if line:  # Check if the line is not empty
+        # Split each line into assignment and date, and strip whitespace
+            #assignment, date = line.split(', ')
+            #assignments.append(assignment.strip())
+            #dates.append(date.strip())  
+# Now, we parse the extracted message which contains the assignments and dates.
+# This parsing would depend on the actual format of `extraction_message`. Assuming it's a string with lines like "Assignment name, YYYY-MM-DD".
+# Loop through and add each assignment to the calendar and Google Sheets
+    #for assignment, date in zip(assignments, dates):
+        #gcalendar.add_calendar_event(assignment, date)
+        #gsheets.add_assignment(assignment, date)
+    #print(messages)
 # Example usage
-    gcalendar.add_calendar_event("P2M5","2023-12-10")
-    gsheets.add_assignment("P2M5","2023-12-10")
-
-def analyze_pdf(file_path):
-    # Read the PDF
-    #pdf_text = read_pdf(file_path)
-
-    # Use OpenAI to extract information (replace 'your-model' with the appropriate model name)
-    response = openai.completions.create(
-        model="text-davinci-003",  # Choose the engine you want to use
-        prompt="Here is the syllabus text: Schedule See the section titled Course Locations and Timing to know on which dates there are in-class sessions. All actual due dates are posted in D2L. Some items listed here are earlier and prompt you to get moving and are relative to when they are actually due in D2L, which might be a day or more later. ALL in-person session dates for the hybrid section are teal and bold. Aug 14: Get code and Use it: Application Development = Coding + Management, VideoQuiz1: Watch the intro video. - setup Python and Teams, Ex1. Turn in IDLE screenshot; Monday Aug 18: Get code and Use. \n\n Question: Show me the assignment names and the days they are due, formatted with Assignents and dates as two columns",
-
-        #prompt= "What class is this?",
-        max_tokens=1000  # Adjust as needed based on your text length
-    )
-    #response = openai.completions.create(
-        #model="text-davinci-003",  # Choose the engine you want to use
-        #prompt="Hello my name is zach and this is sample text. August 28th Test 4. IS 3020: Application Development",
-        #prompt= "Show me the assignment names and the days they are due",
-        #max_tokens=4000  # Adjust as needed based on your text length
-    #)
-    # Extract information from OpenAI response (modify as per your use case)
-    #extracted_info = response['choices'][0]['text']
-
-    # Display or process the extracted information
-    print(response.choices[0].text.strip())
+    #gcalendar.add_calendar_event("P2M5","2023-12-10")
+    #gsheets.add_assignment("P2M5","2023-12-10")
 
 def browse_file(entry_var):
     file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf"), ("All files", "*.*")])
@@ -130,5 +196,4 @@ button = tkinter.Button(frame, text="Read PDF", command=import_files)
 button.grid(row=1, column=0, sticky="news", padx=20, pady=10)
 
 window.mainloop()
-
 
