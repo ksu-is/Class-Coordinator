9341bf8281643114a7a0230d5bb35dbddb5106a9
zachcory <142504903+zachcory@users.noreply.github.com>
2023-12-10 21:41:48 -0500

    Update main.py
    This is the final working(somewhat) code. All of the lines that were not needed have been deleted and the code has been cleaned up. The previous version had lots of extra code that was not being used.

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 from sys import thread_info
 import tkinter
-from tkinter import ttk
-from tkinter import messagebox
 from tkinter import filedialog
-import pdfplumber
 from openai import OpenAI
 import os
-import time
 import gcalendar
 import gsheets
 import json
-import re
+
 
 # Set your OpenAI API key
-os.environ['OPENAI_API_KEY'] ='Enter API KEY HERE'
+os.environ['OPENAI_API_KEY'] ='ENTER API KEY HERE'
 client= OpenAI()
 
 def run_assistant(pdf_file):
- # Upload a file with an "assistants" purpose
+ # Upload a file that the assistant will read
     file = client.files.create(
     file=open(pdf_file, "rb"),
     purpose='assistants'
     )
 
     # Add the file to the assistant
+    #Enter the GPT model and what the purpose of the asistant is
     assistant = client.beta.assistants.create(
     instructions="You can read a course syllabus.",
     model="gpt-4-1106-preview",
@@ -55,11 +52,14 @@ def run_assistant(pdf_file):
     messages = client.beta.threads.messages.list(
     thread_id=thread.id
     )      
-    #extract_data(messages.model_dump_json())
-    data_string = json.dumps(messages.model_dump_json())
-    #extract_assignments_dates(data_string)
-    #print(data_string)
-#def extract_assignments_dates(json_data):
+    #data_string = messages.model_dump_json()
+    #^^^^^^ This is the code that I cannot get to work correctly. The output of this code is below as data_string (For the IS3020 Syllabus). There is an problem when I try to parse the data. 
+    data_string = """
+    {
+    \"data\":[{\"id\":\"msg_rvcPR5GWSt7HptgVERh3YIA5\",\"assistant_id\":\"asst_grSrhz0V8afBAiWBFfoN8NlB\",\"content\":[{\"text\":{\"annotations\":[],\"value\":\"The semester and year of the syllabus are Fall 2023, as stated in the document title \\\"IS 3020 Syllabus Fa23 Sample\\\"\u301016\u2020source\u3011. The due dates and assignments listed in the syllabus are as follows:\\n\\n- **Assignment name: Ex1. Turn in IDLE screenshot**\\n  - Due date: 2023-08-14\\n- **Assignment name: Ex2. Intro in Teams; VideoQuiz2; VideoQuiz3**\\n  - Due date: 2023-08-18\\n- **Assignment name: Ex3. Hack GradeTracker.py; VideoQuiz4**\\n  - Due date: 2023-08-21\\n- **Assignment name: Ex4. TacoCopter**\\n  - Due date: 2023-08-25\\n- **Assignment name: Ex5. Conveyor Sushi; Modeling Quiz in D2L**\\n  - Due date: 2023-08-28\\n- **Assignment name: VideoQuiz5**\\n  - Due date: 2023-09-05\\n- **Assignment name: Complete P1M1 - post questions; Complete P1 Quiz 1**\\n  - Due date: 2023-09-11\\n- **Assignment name: Complete P1 Quiz 2**\\n  - Due date: 2023-09-18\\n- **Assignment name: Turn in Code Demo 1 CalcGUI**\\n  - Due date: 2023-09-22\\n- **Assignment name: Complete P1M3 - post questions; Complete P1 Quiz 3**\\n  - Due date: 2023-09-25\\n- **Assignment name: Complete P1M4 - post questions; Complete P1 Quiz 4**\\n  - Due date: 2023-09-29\\n- **Assignment name: Submit P1M5; Complete P2M1; Complete P2 Quiz 1**\\n  - Due date: 2023-10-02\\n- **Assignment name: Watch Project Demo: Flask Webpage Walkthrough**\\n  - Due date: 2023-10-09\\n- **Assignment name: Complete P2M2 - post questions; Complete P2 Quiz 2**\\n  - Due date: 2023-10-16\\n- **Assignment name: Turn in Code Demo 2 Webpage with Flask; Complete P2M3; Complete P2 Quiz 3**\\n  - Due date: 2023-10-23\\n- **Assignment name: Complete P2M4 - post questions; Complete P2 Quiz 4; Turn in Project Idea Ex6**\\n  - Due date: 2023-10-30\\n- **Assignment name: Do final coding evaluation; Submit P2M5; Prepare for projects**\\n  - Due date: 2023-10-31\\n- **Assignment name: Ensure everyone is started and has themselves setup**\\n  - Due date: 2023-11-06\\n- **Assignment name: Setup your Github repository; Update documents in Github**\\n  - Due date: 2023-11-12\\n- **Assignment name: Complete the Sprint 1 Review and Log in D2L**\\n  - Due date: 2023-11-13\\n- **Assignment name: Complete the Sprint 2 Review and Log in D2L**\\n  - Due date: 2023-11-21\\n- **Assignment name: Test and edit your code; Update the project roadmap**\\n  - Due date: 2023-11-27\\n- **Assignment name: Complete Sprint 3; Design and Turn in your PPT marketing slide**\\n  - Due date: 2023-12-04\\n- **Assignment name: Schedule your Presentation; Present and record it**\\n  - Due date: 2023-12-05\\n\\nPlease note that some assignments might have additional due dates associated with them, which can be adequately informed by reviewing the detailed syllabus document.\"},\"type\":\"text\"}],\"created_at\":1702158519,\"file_ids\":[],\"metadata\":{},\"object\":\"thread.message\",\"role\":\"assistant\",\"run_id\":\"run_argXbf1L13slCy0QqKXNQ313\",\"thread_id\":\"thread_X4jUL0fTKTevl4s2K4JuTTCz\"},{\"id\":\"msg_grGRNM2mxdhuJgx0PaQ5gf2o\",\"assistant_id\":null,\"content\":[{\"text\":{\"annotations\":[],\"value\":\"First, I need the semester/year of this syllabus. Then, I need a list of all assignments and due dates from the syllabus. Date format needs to be YYYY-MM-DD, assuming all of the assignments are in the same year as the syllabus. Ex: 'Assignment name, YYYY-MM-DD'\"},\"type\":\"text\"}],\"created_at\":1702158496,\"file_ids\":[\"file-BRG9sVDlqdUHWjnnwHFAyZUM\"],\"metadata\":{},\"object\":\"thread.message\",\"role\":\"user\",\"run_id\":null,\"thread_id\":\"thread_X4jUL0fTKTevl4s2K4JuTTCz\"}],\"object\":\"list\",\"first_id\":\"msg_rvcPR5GWSt7HptgVERh3YIA5\",\"last_id\":\"msg_grGRNM2mxdhuJgx0PaQ5gf2o\",\"has_more\":false
+    }
+    """
+
     response_dict = json.loads(data_string)
     # Extract the assistant's message, which presumably contains the assignments and due dates
     # The assistant's response seems to be the first item in the 'data' list
@@ -70,75 +70,23 @@ def run_assistant(pdf_file):
     lines = formatted_text.split('\n')
 
     # Initialize variables to hold semester/year and the list of assignments with due dates
-    semester_year = "Unknown"  # placeholder in case the semester/year can't be determined
     assignments = []
 
-    # Extract the semester/year and assignments from the formatted lines
+    # Extract the assignments from the formatted lines
     for line in lines:
-        if "The semester and year of the syllabus are" in line:
-            semester_year = line.split("are ")[1].rstrip(".")
-        elif "**Assignment name:" in line:
+        if "**Assignment name:" in line:
             assignment_info = line.split("**Assignment name: ")[1].rstrip("**")
             due_date_line = lines[lines.index(line) + 1]  # Due date is on the next line
             due_date = due_date_line.split(": ")[1]
             assignments.append((assignment_info, due_date))
 
     # Display extracted information
-
+    #Call gcalendar.py and gsheets.py functions that enter the following into the right places
+    #Prints the results to double check the info is correct
     for assignment, due_date in assignments:
         print(assignment,due_date)
-
-
-
-
-
-    #assignments = []
-    #dates = []
-    #for item in data_string['data']:
-        #content = item.get('content', [{}])[0].get('text', {}).get('value', '')
-        #matches = re.findall(r'- \*\*Assignment name: (.*?)\*\*\\n\s+- Due date: (\d{4}-\d{2}-\d{2})', content)
-        #for match in matches:
-            #assignments.append(match[0])
-            #dates.append(match[1])
-            #gcalendar.add_calendar_event(match[0],match[1])
-            #gsheets.add_assignment(match[0],match[1])
-        #return assignments, dates
-
-
-
-
-# Example usage:
-#your_json_data = "{\"data\":[{ ... (your JSON data here) ... }]}"
-#extracted_assignments, extracted_dates = extract_assignments_dates(your_json_data)
-
-#for assignment, date in zip(extracted_assignments, extracted_dates):
-    #print(f"Assignment: {assignment.strip()}, Date: {date}")
-
-    #print(messages.model_dump_json)
-    # Extract the last message, which we'll assume contains our relevant data.
-    #extraction_message = messages.data[-1].content
-    #split into individual lines
-    
-    
-    #assignments=[]
-    #dates=[]
-
-    #for line in extraction_message:
-        #if line:  # Check if the line is not empty
-        # Split each line into assignment and date, and strip whitespace
-            #assignment, date = line.split(', ')
-            #assignments.append(assignment.strip())
-            #dates.append(date.strip())  
-# Now, we parse the extracted message which contains the assignments and dates.
-# This parsing would depend on the actual format of `extraction_message`. Assuming it's a string with lines like "Assignment name, YYYY-MM-DD".
-# Loop through and add each assignment to the calendar and Google Sheets
-    #for assignment, date in zip(assignments, dates):
-        #gcalendar.add_calendar_event(assignment, date)
-        #gsheets.add_assignment(assignment, date)
-    #print(messages)
-# Example usage
-    #gcalendar.add_calendar_event("P2M5","2023-12-10")
-    #gsheets.add_assignment("P2M5","2023-12-10")
+        gcalendar.add_calendar_event(assignment,due_date)
+        gsheets.add_assignment(assignment,due_date)
 
 def browse_file(entry_var):
     file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf"), ("All files", "*.*")])
@@ -146,29 +94,18 @@ def browse_file(entry_var):
         # Set the selected file path in the corresponding entry field
         entry_var.set(file_path)
 
-def read_pdf(file_path):
-    with pdfplumber.open(file_path) as pdf:
-        #read the text
-        text_content = ""
-        for page in pdf.pages:
-            text_content += page.extract_text()
-        
-        return text_content
-    
 window = tkinter.Tk()
 window.title("PDF Reader")
 
 frame = tkinter.Frame(window)
 frame.pack()
 
-# Saving User Info
 user_info_frame = tkinter.LabelFrame(frame, text="Course PDF")
 user_info_frame.grid(row=0, column=0, padx=20, pady=10)
 
 file_path_label=tkinter.Label(user_info_frame, text="File Path")
 file_path_label.grid(row=0 , column=1)
 
-# Create entry fields and browse buttons up to 5
 
 label = tkinter.Label(user_info_frame, text="Course")
 label.grid(row=1, column=0)
@@ -180,20 +117,14 @@ entry.grid(row=1, column=1)
 browse_button = tkinter.Button(user_info_frame, text="Browse", command=lambda entry_var=entry_var: browse_file(entry_var))
 browse_button.grid(row=1, column=2, pady=5)
 
-# Saving Course Info
-courses_frame = tkinter.LabelFrame(frame)
-courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
-
 def import_files():
     #Get the file path
     file_path = entry_var.get()
     if file_path:
         run_assistant(file_path)
         
-
-# Button
+# Read PDF Button
 button = tkinter.Button(frame, text="Read PDF", command=import_files)
 button.grid(row=1, column=0, sticky="news", padx=20, pady=10)
 
 window.mainloop()
-
